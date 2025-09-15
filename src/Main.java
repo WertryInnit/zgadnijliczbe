aa8177481ea46e86749c0a9d6f97133abb0ed152
WertryInnit <wertryyoung@gmail.com>
2025-09-15 14:31:30 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("noelo");
+        //typy proste
+        //zmienne nie obiekty- pisane małymi literami
+        //typy całkowite
+        byte liczba=120;
+        short liczba2=300;
+        int liczba3=123123;
+        long liczba4=321321321;
+        //System.out.println("liczba Całkowite "+liczba4);
+        //typy zmiennoprzecinkowe
+        float liczba5=12.121f;
+        double liczba6=12.12321;
+        //typy logiczne
+        boolean toczynie=true;
+        //typ znakowy
+        char znak='a';
+        /*
+        * napisz program w którym zgadniesz liczbe wylosowana z zakresu od 1-100
+        *
+        * */
+
+
+
+
+
+
+
+
     }
 }
